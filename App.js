c8f492dd7d76f4000f0d92405900ee0d4d081199
OlenaHras <alena.hras@gmail.com>
2023-04-12 21:02:21 +0300

    add form

--- App.js
+++ App.js
@@ -1,10 +1,68 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+} from "react-native";
+
+// const image = { require: "./assets/images/bgImage.png" };
 
 export default function App() {
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>App.js </Text>
+      <ImageBackground
+        source={require("./assets/images/bgImage.png")}
+        style={styles.bgImage}
+      >
+        <View style={styles.form}>
+          <View style={{ marginBottom: isShowKeyboard ? 32 : 109 }}>
+            <KeyboardAvoidingView
+              behavior={Platform.OS == "ios" ? "padding" : "height"}
+            >
+              <TextInput
+                style={styles.input}
+                placeholder="Логин"
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Адрес электронной почты"
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+              <TextInput
+                style={styles.input}
+                placeholder="Пароль"
+                secureTextEntry={true}
+                onFocus={() => setIsShowKeyboard(true)}
+              />
+            </KeyboardAvoidingView>
+          </View>
+          <TouchableOpacity
+            onPress={handleButtonClick}
+            style={styles.button}
+            activeOpacity={0.8}
+          >
+            <Text style={styles.btnTitle}>Зарегистрироваться</Text>
+          </TouchableOpacity>
+        </View>
+        {/* <Button title="Зарегистрироваться" /> */}
+        {/* <Text style={styles.text}>App.js </Text> */}
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +72,40 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  bgImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  form: {
+    width: 343,
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
+    // marginHorizontal: 16,
+    // width: 343,
+    height: 50,
+    padding: 16,
+    marginBottom: 16,
+  },
+  button: {
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    height: 51,
+    marginTop: 43,
     alignItems: "center",
     justifyContent: "center",
+  },
+  btnTitle: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    lineHeight: 19,
   },
 });
